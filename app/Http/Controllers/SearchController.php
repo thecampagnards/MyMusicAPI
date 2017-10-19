12c0afd9a11f14f9f8d3e490ede2bf08b331c6be
Konstantin <konstantin.sidorenko@orange.fr>
2017-10-19 12:18:21 +0200

    [SEARCH] - Add search option on MyMusic, Youtube and SoundCloud

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Client;
+
+class SearchController extends Controller
+{
+  public function show($search){
+    try{
+    
+      // encodage pour les requetes sur YouTube ou SoundCloud
+      $searchEncode   = urlencode($search);
+      $musiques = new \StdClass();
+      // client de requete
+      $client = new Client();
+      
+      // SoundCloud
+      $promiseSoundCloud = $client->requestAsync('GET', 'http://api.soundcloud.com/tracks?q='.$searchEncode.'&client_id='.env('SOUND_CLOUD_CLIENT_ID'));
+      $promiseSoundCloud->then(function($response) use (&$musiques){
+        $datas = json_decode($response->getBody());
+        foreach ($datas as $data){
+          // on rempli notre objet musique
+          $musique->title         = $data->title;
+          $musique->artist        = $data->user->username;
+          $musique->length        = round($data->duration / 1000);
+          $musique->url           = $data->stream_url . '?client_id=' . env('SOUND_CLOUD_CLIENT_ID');
+          $musique->permaurl      = $data->permaurl;
+          $musique->cover         = $data->artwork_url;
+          $musiques->SoundCloud[] = $musique;
+        }
+      });
+      
+      // YouTube
+      $promiseYouTube = $client->requestAsync('GET', ' https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q='.$searchEncode.'&key='.env('YOUTUBE_API_KEY'));
+      $promiseYouTube->then(function($response) use (&$musiques){
+        $datas = json_decode($response->getBody());
+        foreach ($datas['items'] as $data){
+          // on rempli notre objet musiques
+          $musique->videoId    = $data->id->videoId;
+          $musique->title      = $data->snippet->title;
+          $musique->artist     = $data->snippet->channelTitle;
+          $musique->cover      = $data->thumbnails->default->url;
+          $musiques->YouTube[] = $musique;
+        }
+      });
+      
+      // MyMusic
+      $musiques->MyMusic = DB::table('MUSIQUE')
+        ->where('artist', 'like', '%'.$search.'%')
+        ->orWhere('title', 'like', '%'.$search.'%')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+      // on les parcours
+      foreach ($musiques->MyMusic as &$musique){
+          // On ajoute les fichiers mp3 et cover
+          $this->addFiles($musique);
+      }
+      
+      // On attend la fin du traitement des requetes
+      while ($promiseSoundCloud->wait() && $promiseYouTube->wait());
+      
+      return response()->json($musiques);
+    }catch (Exception $e){
+      return response()->json($e);
+    }
+  }
+}
