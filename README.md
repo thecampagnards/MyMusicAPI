6098c3f6dd2dd3151203f0256745fa686a3be1df
TheCampagnards <konstantin.sidorenko@orange.fr>
2017-10-19 21:20:01 +0200

    [SEARCH] - Fix multiple bugs

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 Instructions :
 
 - Installer ```python```,  ```pip``` puis ```eyeD3```
+- Ajouter le dépôt ```deb http://ftp.uk.debian.org/debian jessie-backports main```
+- Intsaller les paquets suivants ```apt-get install jq youtube-dl ffmpeg lame```
 - ```composer install```
 - ```php artisan jwt:secret```
 - Installer la base de données
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,43 +8,47 @@ class SearchController extends Controller
 {
   public function show($search){
     try{
-    
+
       // encodage pour les requetes sur YouTube ou SoundCloud
       $searchEncode   = urlencode($search);
       $musiques = new \StdClass();
       // client de requete
       $client = new Client();
-      
+
       // SoundCloud
       $promiseSoundCloud = $client->requestAsync('GET', 'http://api.soundcloud.com/tracks?q='.$searchEncode.'&client_id='.env('SOUND_CLOUD_CLIENT_ID'));
       $promiseSoundCloud->then(function($response) use (&$musiques){
         $datas = json_decode($response->getBody());
         foreach ($datas as $data){
           // on rempli notre objet musique
+          $musique = new \StdClass();
           $musique->title         = $data->title;
           $musique->artist        = $data->user->username;
           $musique->length        = round($data->duration / 1000);
-          $musique->url           = $data->stream_url . '?client_id=' . env('SOUND_CLOUD_CLIENT_ID');
+          $musique->url           = $data->stream_url.'?client_id='.env('SOUND_CLOUD_CLIENT_ID');
           $musique->permalink_url = $data->permalink_url;
           $musique->cover         = $data->artwork_url;
           $musiques->SoundCloud[] = $musique;
         }
       });
-      
+
       // YouTube
-      $promiseYouTube = $client->requestAsync('GET', ' https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q='.$searchEncode.'&key='.env('YOUTUBE_API_KEY'));
+      $promiseYouTube = $client->requestAsync('GET', 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q='.$searchEncode.'&key='.env('YOUTUBE_API_KEY'));
       $promiseYouTube->then(function($response) use (&$musiques){
         $datas = json_decode($response->getBody());
-        foreach ($datas['items'] as $data){
-          // on rempli notre objet musiques
-          $musique->videoId    = $data->id->videoId;
-          $musique->title      = $data->snippet->title;
-          $musique->artist     = $data->snippet->channelTitle;
-          $musique->cover      = $data->thumbnails->default->url;
-          $musiques->YouTube[] = $musique;
+        foreach ($datas->items as $data){
+          if($data->id->kind === 'youtube#video'){
+            // on rempli notre objet musiques
+            $musique = new \StdClass();
+            $musique->videoId    = $data->id->videoId;
+            $musique->title      = $data->snippet->title;
+            $musique->artist     = $data->snippet->channelTitle;
+            $musique->cover      = $data->snippet->thumbnails->default->url;
+            $musiques->YouTube[] = $musique;
+          }
         }
       });
-      
+
       // MyMusic
       $musiques->MyMusic = DB::table('MUSIQUE')
         ->where('artist', 'like', '%'.$search.'%')
@@ -57,10 +61,9 @@ class SearchController extends Controller
           // On ajoute les fichiers mp3 et cover
           $this->addFiles($musique);
       }
-      
+
       // On attend la fin du traitement des requetes
-      while ($promiseSoundCloud->wait() && $promiseYouTube->wait());
-      
+      $promiseSoundCloud->wait() && $promiseYouTube->wait();
       return response()->json($musiques);
     }catch (Exception $e){
       return response()->json($e);
